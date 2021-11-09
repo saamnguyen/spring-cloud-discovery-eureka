9f6e0233e0bc0ddc3466f5760e80088b89c65f26
samukma <nguyensam576@gmail.com>
2021-11-09 18:53:55 +0700

    create project

---
+++ src/main/java/com/kitclub/springclouddiscoveryeureka/SpringCloudDiscoveryEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.kitclub.springclouddiscoveryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDiscoveryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDiscoveryEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kitclub/springclouddiscoveryeureka/SpringCloudDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitclub.springclouddiscoveryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDiscoveryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
